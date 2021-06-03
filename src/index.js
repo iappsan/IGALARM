5e216824aa1a2015f95363fc482786e568c6db5d
Ian SR <iann.ipn9@gmail.com>
2021-06-03 03:29:17 -0500

    main carousel

--- src/index.js
+++ src/index.js
@@ -4,10 +4,12 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 import './index.css';
 import MyNavbar from './components/MyNavbar';
+import MainCarousel from './components/MainCarousel';
 
 ReactDOM.render(
   <>
     <MyNavbar/>
+    <MainCarousel/>
   </>
   ,
   document.getElementById('root')
