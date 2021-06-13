0481c5f80a88a00f1bd25482e481346e033f2ebc
Ian SR <iann.ipn9@gmail.com>
2021-06-13 05:52:25 -0500

    del login error

--- src/App.js
+++ src/App.js
@@ -90,7 +90,6 @@ const App = () => {
                 console.log("prop " + user.email)
             })
             .catch((error) => {
-                console.log(error)
                 switch (error.code) {
                     default:
                     case "auth/invalid-email":
